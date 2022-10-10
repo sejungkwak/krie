a4966ec9c1c3ff8e078f72fc62462a24259ff1bc
Sejung Kwak <kwak.sejung@gmail.com>
2022-10-10 21:27:12 +0000

    feat: Create profile model and run migrations

--- users/models.py
+++ users/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.dispatch import receiver
+from django.db.models.signals import post_save
 
-# Create your models here.
+
+class Profile(models.Model):
+    """
+    User profile database model
+    """
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+        primary_key=True)
+    location = models.CharField(max_length=20, blank=True, null=True)
+    bio = models.CharField(max_length=200, blank=True, null=True)
+
+    def __str__(self):
+        return self.user.username
+
+
+@receiver(post_save, sender=User)
+def update_profile_signal(sender, instance, created, **kwargs):
+    """
+    Create a profile for the new user.
+    """
+    if created:
+        Profile.objects.create(user=instance)
+    instance.user.save()
