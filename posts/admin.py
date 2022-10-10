1fde62e492227a2827cce1fc97de6ef03f4bbf37
Sejung Kwak <kwak.sejung@gmail.com>
2022-10-10 22:37:54 +0000

    feat: Create posts view for admin page

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,40 @@
 from django.contrib import admin
+from .models import Category, Post, Comment
 
-# Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    """
+    Category panel for the admin site
+    """
+    list_display = ('name',)
+    search_fields = ['name']
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    """
+    Post panel for the admin site
+    """
+    list_display = (
+        'category',
+        'title',
+        'author',
+        'created_on',
+        'updated_on')
+    search_fields = ['author', 'title']
+    actions = ['delete_selected']
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    """
+    Comment panel for the admin site
+    """
+    list_display = (
+        'original_post',
+        'author',
+        'created_on',
+        'updated_on')
+    search_fields = ['author']
+    actions = ['delete_selected']
