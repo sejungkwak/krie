07cc7639b298a87c9bfcde4c7760cf867aedfe7f
Sejung Kwak <kwak.sejung@gmail.com>
2022-10-10 21:28:02 +0000

    feat: Create profile view for admin page

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Profile
 
-# Register your models here.
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    """
+    Profile view in the admin panel
+    """
+    model = Profile
+    list_display = ('username', 'email', 'date_joined')
+
+    def username(self, obj):
+        return obj.user.username
+
+    def email(self, obj):
+        return obj.user.email
+
+    def date_joined(self, obj):
+        return obj.user.date_joined
