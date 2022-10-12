690b2467c80867329bc51c77fdc13f29e4cf437e
Sejung Kwak <kwak.sejung@gmail.com>
2022-10-12 21:03:03 +0000

    feat: Add url path for the posts app

--- krie/urls.py
+++ krie/urls.py
@@ -18,7 +18,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('home.urls')),
     path('accounts/', include('allauth.urls')),
     path('summernote/', include('django_summernote.urls')),
+    path('', include('home.urls')),
+    path('posts/', include('posts.urls')),
 ]
