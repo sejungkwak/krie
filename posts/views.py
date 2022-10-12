a2caf8722f5f9770b9b8129c955ee67261b69431
Sejung Kwak <kwak.sejung@gmail.com>
2022-10-12 21:29:05 +0000

    feat: Create view for creating a post

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic.edit import CreateView
+from .models import Post
+from .forms import PostForm
 
-# Create your views here.
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    template_name = 'posts/create_post.html'
+    fields = ['category', 'title', 'body']
