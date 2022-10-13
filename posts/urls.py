2243951b25d5db1316c1233750a59f5c35f5af1b
Sejung Kwak <kwak.sejung@gmail.com>
2022-10-13 21:41:17 +0000

    feat: Add url pattern for post detail

--- posts/urls.py
+++ posts/urls.py
@@ -2,5 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('create_post/', views.PostCreateView.as_view(), name='create_post'),
+    path(
+        'create_post/',
+        views.PostCreateView.as_view(),
+        name='create_post'),
+    path(
+        '<str:category_name>/<int:post_id>/',
+        views.PostDetailView.as_view(),
+        name='read_post'),
 ]
