f170214b2affe67e35ab60f9aaf88a0c219940ba
Sejung Kwak <kwak.sejung@gmail.com>
2022-10-15 16:23:34 +0000

    feat: Add url path for post list view

--- posts/urls.py
+++ posts/urls.py
@@ -3,20 +3,20 @@ from . import views
 
 urlpatterns = [
     path(
-        '<int:category_id>/',
-        views.PostListView.as_view(),
-        name='post_list'
-    ),
-    path(
         'create_post/',
         views.PostCreateView.as_view(),
         name='create_post'),
     path(
-        '<str:category_name>/<int:post_id>/',
+        '<slug:category_slug>/<int:post_id>/',
         views.PostDetailView.as_view(),
         name='read_post'),
     path(
-        '<str:category_name>/<int:pk>/delete_post/',
+        '<slug:category_slug>/<int:pk>/delete_post/',
         views.PostDeleteView.as_view(),
         name='delete_post'),
+    path(
+        '<slug:category_slug>/',
+        views.PostListView.as_view(),
+        name='post_list'
+    ),
 ]
