73b8724c4ffbd5c45b32880334db2c5aa2f34fa0
Sejung Kwak <kwak.sejung@gmail.com>
2022-10-15 17:42:26 +0000

    feat: Add comments count method to post model

--- posts/models.py
+++ posts/models.py
@@ -44,6 +44,9 @@ class Post(models.Model):
     def number_of_likes(self):
         return self.likes.count()
 
+    def number_of_comments(self):
+        return self.comments.count()
+
 
 class Comment(models.Model):
     """
