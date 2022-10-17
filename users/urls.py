2c53fffc8a8c81c24b167ed111126f8fe856b024
Sejung Kwak <kwak.sejung@gmail.com>
2022-10-17 21:53:05 +0000

    feat: Add profile view path

---
+++ users/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path(
+        'profile/<int:pk>/',
+        views.ProfileDetailView.as_view(),
+        name='read_profile'),
+]
