cfd1b659e5573b71837700abef8230c57096e146
Sejung Kwak <kwak.sejung@gmail.com>
2022-10-20 21:46:31 +0000

    feat: Add search results page url path

--- home/urls.py
+++ home/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.get_home, name='home')
+    path('', views.get_home, name='home'),
+    path('search_results/', views.SearchView.as_view(), name='search_results')
 ]
