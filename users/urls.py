6f041f417cc442e6679710fe7492a32cd75a4b2c
Sejung Kwak <kwak.sejung@gmail.com>
2022-10-24 12:16:16 +0000

    feat: Add url path to update profile view

--- users/urls.py
+++ users/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path(
-        'profile/<int:pk>/',
-        views.ProfileDetailView.as_view(),
-        name='read_profile'),
+    path('profile/<int:pk>/',
+         views.ProfileDetailView.as_view(),
+         name='read_profile'),
+    path('profile/<int:pk>/edit/',
+         views.ProfileUpdateView.as_view(),
+         name='update_profile')
 ]
