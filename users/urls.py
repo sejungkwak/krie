1cfd9ee2339873a323675e3e08f6764690f7b08a
Sejung Kwak <kwak.sejung@gmail.com>
2022-10-26 17:33:49 +0000

    refactor: Refactor a single method for the profile view to separate methods

--- users/urls.py
+++ users/urls.py
@@ -2,9 +2,15 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('profile/<int:pk>/',
-         views.ProfileDetailView.as_view(),
-         name='read_profile'),
+    path('profile/<int:pk>/activity/posts/',
+         views.ProfilePostListView.as_view(),
+         name='read_profile_posts'),
+    path('profile/<int:pk>/activity/comments/',
+         views.ProfileCommentListView.as_view(),
+         name='read_profile_comments'),
+    path('profile/<int:pk>/about/',
+         views.ProfileAboutView.as_view(),
+         name='read_profile_about'),
     path('profile/<int:pk>/edit/',
          views.ProfileUpdateView.as_view(),
          name='update_profile')
--- users/views.py
+++ users/views.py
@@ -1,48 +1,104 @@
 from django.shortcuts import render, get_object_or_404
-from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib import messages
+from django.contrib.messages.views import SuccessMessageMixin
 from django.http import HttpResponseRedirect
 from django.urls import reverse_lazy
 from django.views.generic import UpdateView
+from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
 from posts.models import Post, Comment
 from .models import Profile
 from .forms import UserUpdateForm, ProfileUpdateForm
 
 
-class ProfileDetailView(LoginRequiredMixin, ListView):
+class ProfileAboutView(LoginRequiredMixin, DetailView):
     """
-    Profile view for users to view their own profile and others
+    Display a user's location and bio.
     """
-
     model = Profile
     template_name = 'users/read_profile.html'
 
     def get_context_data(self, **kwargs):
+        """
+        Retrieve the specified user's profile data.
+        """
+        context = super().get_context_data(**kwargs)
+        context['profile'] = Profile.objects.get(pk=self.kwargs['pk'])
+        return context
+
+
+class ProfilePostListView(LoginRequiredMixin, ListView):
+    """
+    Display a list of posts written by the specified user.
+    """
+    model = Post
+    template_name = 'users/read_profile_posts.html'
+    paginate_by = 10
+
+    def get_queryset(self):
+        """
+        Get a user to display.
+        """
+        return super().get_queryset().filter(author=self.kwargs['pk'])
+
+    def get_context_data(self, **kwargs):
+        """
+        Retrieve a list of posts written by the specified user.
+        """
         context = super().get_context_data(**kwargs)
         context['profile'] = Profile.objects.get(pk=self.kwargs['pk'])
         context['posts'] = Post.objects.filter(
             author=self.kwargs['pk']).order_by('-created_on')
+        return context
+
+
+class ProfileCommentListView(LoginRequiredMixin, ListView):
+    """
+    Display a list of comments written by the specified user.
+    """
+    model = Comment
+    template_name = 'users/read_profile_comments.html'
+    paginate_by = 6
+
+    def get_queryset(self):
+        """
+        Get a user to display.
+        """
+        return super().get_queryset().filter(author=self.kwargs['pk'])
+
+    def get_context_data(self, **kwargs):
+        """
+        Retrieve a list of comments written by the specified user.
+        """
+        context = super().get_context_data(**kwargs)
+        context['profile'] = Profile.objects.get(pk=self.kwargs['pk'])
         context['comments'] = Comment.objects.filter(
             author=self.kwargs['pk']).order_by('-created_on')
         return context
 
 
-class ProfileUpdateView(UserPassesTestMixin, UpdateView):
+class ProfileUpdateView(UserPassesTestMixin, SuccessMessageMixin, UpdateView):
     """
-    Profile update view for users to edit their own profile
+    Edit a user's own profile.
     """
-
     model = Profile
     user_form_class = UserUpdateForm
     profile_form_class = ProfileUpdateForm
     template_name = 'users/update_profile.html'
+    success_message = 'Your profile has been successfully updated.'
 
     def test_func(self):
+        """
+        Check if the logged-in user is the owner of the account.
+        """
         if self.get_object().id == self.request.user.pk:
             return True
 
     def get(self, request, *args, **kwargs):
+        """
+        Retrieve the user's profile data and display current values in the form.
+        """
         id = self.kwargs.get('pk')
         profile = get_object_or_404(Profile, pk=id)
         username = profile.user.username
@@ -57,17 +113,31 @@ class ProfileUpdateView(UserPassesTestMixin, UpdateView):
         })
 
     def post(self, request, *args, **kwargs):
-        user_form = self.user_form_class(data=request.POST, instance=request.user)
-        profile_form = self.profile_form_class(data=request.POST, instance=request.user.profile)
+        """
+        Update the user's profile data.
+        """
+        user_form = self.user_form_class(
+            data=request.POST, instance=request.user)
+        profile_form = self.profile_form_class(
+            data=request.POST, instance=request.user.profile)
 
         if user_form.is_valid() and profile_form.is_valid():
             user_form.save()
             profile_form.save()
-            messages.add_message(request, messages.SUCCESS, 'Your profile has been updated successfully.')
+            messages.success(
+                request, 'Your profile has been successfully updated.')
             return HttpResponseRedirect(self.get_success_url())
         else:
-            messages.add_message(request, messages.ERROR, 'That username is already in use.')
+            messages.add_message(
+                request,
+                messages.ERROR,
+                'That username is already in use.')
         return HttpResponseRedirect(self.get_success_url())
 
     def get_success_url(self):
-        return reverse_lazy('update_profile', kwargs={'pk': self.request.user.pk})
+        """
+        Redirect back to the same page and display the updated data.
+        """
+        return reverse_lazy(
+            'update_profile', kwargs={
+                'pk': self.request.user.pk})
