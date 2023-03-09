4aba310d7531b4e4cd879555adf8f45848e2ca57
Sejung Kwak <kwak.sejung@gmail.com>
2023-03-09 13:04:06 +0000

    build: Add build.sh, Add render to allowed host, Remove Procfile

--- krie/settings.py
+++ krie/settings.py
@@ -32,11 +32,14 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = False
 
 ALLOWED_HOSTS = [
-    'krie.herokuapp.com',
     '127.0.0.1',
     'localhost',
 ]
 
+# Add Render.com URL to allowed hosts
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 # Application definition
 
